48aef64fd0f4b20d23ad15e19396123c22eab4fd
Aryan Pandey <aryanpandey048@gmail.com>
2022-06-09 02:42:22 +0530

    second commit

--- codewithstein/settings.py
+++ codewithstein/settings.py
@@ -121,10 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'Users/Aryan/Downloads/codewithstein-main/codewithstein-main/blog/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'/static/'),
+    os.path.join(BASE_DIR,'blog/static/'),
 ]
+STATIC_ROOT = "C:/Users/Aryan/Downloads/codewithstein-main/codewithstein-main/assets"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
