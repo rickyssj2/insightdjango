7ba704b853056296b8eef434a2988f7a80fb66bd
Aryan Pandey <aryanpandey048@gmail.com>
2022-06-09 02:48:39 +0530

    fourth commit

--- codewithstein/urls.py
+++ codewithstein/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
+from django.urls import include,re_path
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,7 +8,6 @@ from django.conf.urls.static import static
 from blog.views import frontpage, post_detail
 
 from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
@@ -16,8 +15,8 @@ urlpatterns = [
     path('<slug:slug>/', post_detail, name='post_detail'),
     path('summernote/', include('django_summernote.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:
